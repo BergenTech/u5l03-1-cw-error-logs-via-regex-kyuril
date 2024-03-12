33f92a00e9453f4bb281fa2b2e016bf876f08c03
Kyuri <kyulim26@bergen.org>
2024-03-12 09:03:29 -0400

    complete

--- error.py
+++ error.py
@@ -1,1 +1,18 @@
+import json, csv, re 
+with open('regex\\u5l03-1-cw-error-logs-via-regex-kyuril\\logs.json') as file: 
+    data = json.load(file)
 
+    with open('error_logs.csv', 'w+', newline="") as newFile: 
+        writer = csv.writer(newFile)
+        header = ["timestamp", "level", "message"] 
+        writer.writerow(header)
+
+
+        pattern = r"ERROR"
+        #write data into the csv file 
+        for row in data:
+            if re.search(pattern, row["level"], re.IGNORECASE):
+                timestamp = row.get("timestamp")
+                level = "ERROR"
+                message = row.get("message")
+                writer.writerow([timestamp, level, message])
